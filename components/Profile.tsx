40dcb0fb80254d6fc2061e0375882402830859be
JannisMilz <jannis@milz.ch>
2022-11-02 21:12:38 +0000

    WIP NextJS 13 /app folder + PocketBase

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,18 +1,16 @@
-import { useSession } from 'next-auth/react';
 import Image from 'next/image';
 
 export default function Profile() {
-  const { data: session } = useSession();
 
   return (
     <div className="flex items-center">
-      {session?.user?.image ? (
+      {/* {session?.user?.image ? (
         <Image src={session.user.image} alt="Profile image" width={30} height={30} className="rounded-full" />
-      ) : (
+      ) : ( */}
         <Image src="/assets/no-avatar.svg" alt="Profile image" width={30} height={30} className="rounded-full" />
-      )}
+      {/* )} */}
       <p className="flex items-center pl-2 text-black">
-        <span className="hidden md:inline-block">{session?.user?.name}</span>
+        <span className="hidden md:inline-block">Jannis Milz</span>
         <span className="material-icons-round">keyboard_arrow_down</span>
       </p>
     </div>
--- pages/api/auth/README.md
+++
@@ -1,9 +0,0 @@
-# Callbacks
-
-signIn => Control if user is allowed to sign in
-
-redirect => Called when user is redirected to a callback URL (signin or signout)
-
-jwt => Called when JWT session is created / updated
-
-session => Called when a session is checked (e.g. useSession())
