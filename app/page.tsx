530515d64e2a52c6bd272ddf9f8f5aaa3fb80195
JannisMilz <jannis@milz.ch>
2022-11-07 06:06:20 +0000

    google auth implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,31 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import PocketBase from 'pocketbase';
+import { useState } from 'react';
+
+const db = new PocketBase('http://localhost:8090');
+
 export default function HomePage() {
+  const router = useRouter();
+  const redirectUrl = 'http://localhost:3000/redirect';
+  const [user, setUser] = useState(db.authStore.model);
+
+  const redirectLogin = async () => {
+    const { authProviders } = await db.users.listAuthMethods();
+    authProviders.forEach(function (provider) {
+      if (provider.name == 'google') {
+        localStorage.setItem('provider', JSON.stringify(provider));
+
+        router.push(provider.authUrl + redirectUrl);
+      }
+    });
+  };
+
   return (
     <div>
       <h1>Home Page</h1>
-      <p>Some content</p>
+      {user ? <p>Logged in as {user.email}</p> : <button onClick={redirectLogin}>Login</button>}
     </div>
   );
 }
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,26 +1,11 @@
 import PocketBase from 'pocketbase';
 
 const db = new PocketBase('http://pocketbase:8090');
-// const db = new PocketBase('https://pocketbase.io');
-
-// async function getAuthMethods() {
-//   console.log('data');
-
-//   return data;
-// }
 
 export default async function ProjectsPage() {
-  // const data = await db.records.getFullList('projects');
-  // const res = await fetch('http://localhost:8090/api/collections/projects/records', {
-  //   cache: 'no-store',
-  // });
-  // const data = await res.json();
-  // const authMethods = await getAuthMethods();
-  // console.log(data);
-  // console.log(authMethods);
-
   await db.admins.authViaEmail(process.env.POCKETBASE_USERNAME || '', process.env.POCKETBASE_PASSWORD || '');
   const data = await db.records.getFullList('projects');
+  console.log({ client: db.authStore.model });
 
   return <div>Hopefully soon some data: {JSON.stringify(data)}</div>;
 }
