417fe719a134f5fb6e914467998c29d356fbebef
Jannis Milz <jannis.milz@econ.uzh.ch>
2022-11-08 13:20:18 +0000

    Migrate from NextJS to Svelte (TypeScript)

--- components/ProjectCard.tsx
+++
@@ -1,5 +0,0 @@
-import Image from 'next/image';
-
-export default function Logo() {
-  return <div className="flex items-center"></div>;
-}
--- utils/middleware/hello.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default function hello(req: NextApiRequest, res: NextApiResponse, next: any) {
-  console.log('Hello middleware!');
-
-  return next();
-}
