f3bce81a83e840781ebd45b37a18e54bf730d0f5
Jannis Milz <jannis.milz@econ.uzh.ch>
2022-11-09 13:34:49 +0000

    Remove old files

--- old/tailwind.config.cjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./index.html', './src/**/*.{svelte,js,ts}'],
-  darkMode: 'class',
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
