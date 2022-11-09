d31e6578d4dc9dc4d006e37a18b4540df3bb2d56
Jannis Milz <jannis.milz@econ.uzh.ch>
2022-11-09 15:18:17 +0000

    Add custom color palette

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				primary: '#4CA9FF',
+				secondary: '#BCD7FF',
+				success: '#BCD7FF',
+				warning: '#BCD7FF',
+				error: '#BCD7FF',
+				neutral: '#F3F4F6',
+				white: '#FAFAFA',
+				black: '#1C1F21',
+			},
+		},
 	},
-	plugins: []
+	plugins: [],
 };
