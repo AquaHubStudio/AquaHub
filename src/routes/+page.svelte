25719c8a02ebdc6f15ace24854fad7cb7803f7cf
JannisMilz <jannis@milz.ch>
2022-11-13 20:57:19 +0000

    Remove test code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,50 +8,7 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
+<!-- <div class="grid grid-cols-projects grid-rows-projects p-24" /> -->
 <div>
 	<Landing />
-	<div class="grid grid-cols-projects grid-rows-projects p-24">
-		<ProjectCard
-			image="https://picsum.photos/200"
-			title="Test Title"
-			description="Some description for this project so lets type here some more text and hope for the best. I try to write even longer description. I need to find the matching best length."
-			tags={['Tag1', 'Tag2']}
-			progress={23}
-		/>
-		<ProjectCard
-			image="https://picsum.photos/200"
-			title="Test Title"
-			description="Some description for this project so lets type here some more text and hope for the best. I try to write even longer description. I need to find the matching best length."
-			tags={['Tag1', 'Tag2']}
-			progress={23}
-		/>
-		<ProjectCard
-			image="https://picsum.photos/200"
-			title="Test Title"
-			description="Some description for this project so lets type here some more text and hope for the best. I try to write even longer description. I need to find the matching best length."
-			tags={['Tag1', 'Tag2']}
-			progress={23}
-		/>
-		<ProjectCard
-			image="https://picsum.photos/200"
-			title="Test Title"
-			description="Some description for this project so lets type here some more text and hope for the best. I try to write even longer description. I need to find the matching best length."
-			tags={['Tag1', 'Tag2']}
-			progress={23}
-		/>
-		<ProjectCard
-			image="https://picsum.photos/200"
-			title="Test Title"
-			description="Some description for this project so lets type here some more text and hope for the best. I try to write even longer description. I need to find the matching best length."
-			tags={['Tag1', 'Tag2']}
-			progress={23}
-		/>
-		<ProjectCard
-			image="https://picsum.photos/200"
-			title="Test Title"
-			description="Some description for this project so lets type here some more text and hope for the best. I try to write even longer description. I need to find the matching best length."
-			tags={['Tag1', 'Tag2']}
-			progress={23}
-		/>
-	</div>
 </div>
