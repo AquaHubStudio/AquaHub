edc78069adb1005d247d547f777c37ee484e9994
Jannis Milz <jannis.milz@econ.uzh.ch>
2022-11-14 13:57:11 +0000

    WIP Landing page

---
+++ src/lib/components/FeatureDescription.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	export let reversed: boolean = false;
+	export let image: string;
+	export let title: string;
+	export let description: string;
+</script>
+
+<div class="mb-16">
+	<div class={`flex ${reversed ? 'flex-row-reverse' : ''} items-center justify-between gap-24`}>
+		<img src={image} alt="Example" />
+		<div>
+			<h3
+				class="mb-2 w-fit bg-gradient-to-r from-blue-400 to-violet-600 bg-clip-text text-4xl font-extrabold text-transparent"
+			>
+				{title}
+			</h3>
+			<p class="italic text-gray-600">
+				{description}
+			</p>
+		</div>
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <script>
-	import Landing from '$lib/components/Landing.svelte';
-	import ProjectCard from '$lib/components/ProjectCard.svelte';
+	import FeatureDescription from '$lib/components/FeatureDescription.svelte';
 </script>
 
 <svelte:head>
@@ -9,6 +8,47 @@
 </svelte:head>
 
 <!-- <div class="grid grid-cols-projects grid-rows-projects p-24" /> -->
-<div>
-	<Landing />
-</div>
+<section class="m-auto max-w-landing">
+	<div class="flex h-[calc(90vh-64px)] w-full flex-col items-center justify-center text-center">
+		<h1
+			class="bg-gradient-to-r from-blue-400 to-violet-600 bg-clip-text text-8xl font-semibold leading-tight text-white text-transparent"
+		>
+			One Hub. Every Project.
+		</h1>
+		<h5 class="text-xl italic text-gray-600">
+			Share your project. Connect with like-minded founders. Track your progress.
+		</h5>
+		<div class="m-4 flex gap-4">
+			<a href="#" class="CtA-btn bg-gray-300 text-black hover:bg-gray-400 hover:bg-opacity-50">
+				<span class="material-icons-outlined">explore</span>
+				Discover
+			</a>
+			<a href="#" class="CtA-btn bg-indigo-500 text-white hover:bg-indigo-600">
+				<span class="material-icons-round">add</span>
+				Create
+			</a>
+		</div>
+	</div>
+	<FeatureDescription
+		image="https://picsum.photos/400"
+		title="Some title"
+		description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam repellendus, hic cum magnam praesentium rem quis nisi. Nostrum minus id numquam eius alias eaque quibusdam. Suscipit repellat incidunt amet modi."
+	/>
+	<FeatureDescription
+		image="https://picsum.photos/400"
+		title="Some title"
+		description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam repellendus, hic cum magnam praesentium rem quis nisi. Nostrum minus id numquam eius alias eaque quibusdam. Suscipit repellat incidunt amet modi."
+		reversed
+	/>
+	<FeatureDescription
+		image="https://picsum.photos/400"
+		title="Some title"
+		description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam repellendus, hic cum magnam praesentium rem quis nisi. Nostrum minus id numquam eius alias eaque quibusdam. Suscipit repellat incidunt amet modi."
+	/>
+</section>
+
+<style>
+	.CtA-btn {
+		@apply inline-flex items-center gap-3 rounded py-2 px-6 text-base outline-none duration-200;
+	}
+</style>
