1422a1a4c5ed1d21014c13dd60f4003aa54bd358
Jannis Milz <jannis.milz@econ.uzh.ch>
2022-11-14 14:28:22 +0000

    Thicker text

--- src/lib/components/Logo.svelte
+++ src/lib/components/Logo.svelte
@@ -5,6 +5,6 @@
 <div class="flex items-center">
 	<img src="images/logo.png" alt="AquaHub logo" class="h-12 w-12" />
 	{#if title}
-		<p class="text-md pl-2 font-medium text-black">AquaHub</p>
+		<p class="text-md pl-2 font-semibold text-black">AquaHub</p>
 	{/if}
 </div>
