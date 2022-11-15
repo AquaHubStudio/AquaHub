e0ab94e659d197b42fc2cb71e271e93c9cc3f962
Jannis Milz <jannis.milz@econ.uzh.ch>
2022-11-15 11:02:57 +0000

    Make project card clickable. Point to project page

--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -1,33 +1,34 @@
 <script lang="ts">
+	export let id: string;
 	export let image: string;
 	export let title: string;
 	export let description: string;
-	export let tags: string[];
+	export let tags: { title: string }[];
 	export let progress: number;
 </script>
 
 <div class="m-4 rounded-md bg-gray-100 p-3 shadow-md">
-	<div class="flex">
+	<a href={`/projects/${id}`} class="flex">
 		<img src={image} alt="Project logo" class="h-20 w-20 rounded-md" />
-		<div class="px-4">
+		<div class="px-4 text-start">
 			<p class="text-md font-semibold">{title}</p>
 			<p class="max-h-20 overflow-hidden overflow-ellipsis text-xs text-gray-500 line-clamp-3">{description}</p>
 		</div>
-	</div>
+	</a>
 	<div class="mt-2 flex items-center gap-1 pt-2.5 md:flex-col md:items-start md:pt-0">
 		<div class="flex md:pt-2">
 			{#each tags as tag}
 				<p class={`flex items-center gap-1 text-xs rounded-full mr-1.5 px-3 py-0.5 text-gray-700 bg-gray-200`}>
 					<span class="material-icons-round text-xs">sell</span>
-					{tag}
+					{tag.title}
 				</p>
 			{/each}
 		</div>
 		<div class="flex w-full items-center">
 			<div class="h-2.5 w-full rounded-full bg-gray-200">
-				<div class={`bg-blue-500 h-2.5 rounded-full w-[${progress}%]`} />
+				<div class="h-2.5 rounded-full bg-blue-500" style={`width: ${progress}%;`} />
 			</div>
-			<span class="pl-2 text-sm ">{progress}%</span>
+			<span class="pl-2 text-sm text-gray-600">{progress}%</span>
 		</div>
 	</div>
 </div>
