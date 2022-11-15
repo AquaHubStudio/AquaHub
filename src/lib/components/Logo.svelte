a39e759c062fb70b73be549a0489c14d758899e0
Jannis Milz <jannis.milz@econ.uzh.ch>
2022-11-15 11:01:39 +0000

    Logo pointing to home

--- src/lib/components/Logo.svelte
+++ src/lib/components/Logo.svelte
@@ -2,9 +2,9 @@
 	export let title: boolean = true;
 </script>
 
-<div class="flex items-center">
+<a href="/" class="flex items-center">
 	<img src="images/logo.png" alt="AquaHub logo" class="h-12 w-12" />
 	{#if title}
 		<p class="text-md pl-2 font-semibold text-black">AquaHub</p>
 	{/if}
-</div>
+</a>
