ab9564b82136181fdf14e3987406c8971aa2b776
Jannis Milz <jannis.milz@econ.uzh.ch>
2022-11-16 10:09:59 +0000

    Absolute images path

--- src/lib/components/Logo.svelte
+++ src/lib/components/Logo.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <a href="/" class="flex items-center">
-	<img src="images/logo.png" alt="AquaHub logo" class="h-12 w-12" />
+	<img src="/images/logo.png" alt="AquaHub logo" class="h-12 w-12" />
 	{#if title}
 		<p class="text-md pl-2 font-semibold text-black">AquaHub</p>
 	{/if}
--- src/routes/(signin)/signin/+page.svelte
+++ src/routes/(signin)/signin/+page.svelte
@@ -9,7 +9,7 @@
 </svelte:head>
 
 <section class="m-auto flex h-screen flex-col items-center justify-center p-12">
-	<img src="images/logoBannerDark.png" alt="AquaHub banner logo" class="mb-8 max-w-xs" />
+	<img src="/images/logoBannerDark.png" alt="AquaHub banner logo" class="mb-8 max-w-xs" />
 	<div class="w-full rounded-lg bg-white shadow dark:border sm:max-w-md md:mt-0 xl:p-0">
 		<div class="space-y-4 p-6 sm:p-8 md:space-y-6">
 			<h1 class="text-3xl font-bold leading-tight tracking-tight text-gray-900">Sign in</h1>
