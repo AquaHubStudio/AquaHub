9cc298956826c76b133904aaff966853fd2da78d
Jannis Milz <jannis.milz@econ.uzh.ch>
2022-11-16 06:29:25 +0000

    Rename (discover) group to (projects)

--- src/routes/(login)/login/+page.svelte
+++ src/routes/(login)/login/+page.svelte
@@ -8,14 +8,14 @@
 </svelte:head>
 
 <section class="m-auto flex h-screen items-center justify-center">
-	<div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0">
-		<div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+	<div class="w-full rounded-lg bg-white shadow dark:border sm:max-w-md md:mt-0 xl:p-0">
+		<div class="space-y-4 p-6 sm:p-8 md:space-y-6">
 			<h1 class="text-3xl font-bold leading-tight tracking-tight text-gray-900">Login</h1>
 			<div class="flex flex-col justify-between gap-3">
 				<OAuthElement provider="google" />
 				<OAuthElement provider="github" />
 			</div>
-			<a class="flex" href="javascript:history.back()">« Back</a>
+			<a class="flex" href="/">« Back</a>
 		</div>
 	</div>
 </section>
