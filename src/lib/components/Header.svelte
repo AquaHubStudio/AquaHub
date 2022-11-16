082dec74155d11df7e54de4bed99bca3603efc41
Jannis Milz <jannis.milz@econ.uzh.ch>
2022-11-16 12:40:34 +0000

    Add navbar links

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -8,6 +8,10 @@
 <header class="bg-white px-7 py-2 shadow-sm">
 	<nav class={`flex items-center max-w-${maxWidth} justify-between m-auto`}>
 		<Logo />
+		<div class="flex gap-5">
+			<a href="/discover" class="text-gray-800 duration-100 hover:text-gray-500">Discover</a>
+			<a href="#" class="text-gray-800 duration-100 hover:text-gray-500">Create project</a>
+		</div>
 		<Profile />
 	</nav>
 </header>
