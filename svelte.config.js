5fbdb421cfdbe812a9be381107326cc299a11e37
JannisMilz <jannis@milz.ch>
2022-11-22 05:56:38 +0000

    Changes

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,17 @@
-import { sveltekit } from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
 
-/** @type {import('vite').UserConfig} */
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess({
 		postcss: true,
 	}),
-	plugins: [sveltekit()],
-	server: { port: 3000 },
+
+	kit: {
+		adapter: adapter(),
+	},
 };
 
 export default config;
