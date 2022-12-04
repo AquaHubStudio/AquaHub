b762b91aaa81e07de304b055ad66de7b1d899c08
JannisMilz <jannis@milz.ch>
2022-12-04 19:53:36 +0000

    First sketch of project page

--- src/routes/(projects)/projects/[projectId]/+page.svelte
+++ src/routes/(projects)/projects/[projectId]/+page.svelte
@@ -2,7 +2,8 @@
 	import type { PageData } from './$types';
 
 	export let data: PageData;
-	const projectId = data.projectId;
+	const projectId = data.params.projectId;
+	const projectData = data.projectData;
 </script>
 
 <svelte:head>
@@ -10,7 +11,33 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<!-- <div class="grid grid-cols-projects grid-rows-projects p-24" /> -->
-<section class="m-auto max-w-projects">
-	{projectId}
+<section class="m-auto mt-8 max-w-5xl">
+	<div class="flex justify-between">
+		<div class="flex w-1/3 flex-col items-center">
+			{#if projectData.avatar}
+				<img
+					src={`${import.meta.env.VITE_PUBLIC_SERVER_URL}/api/files/projects/${projectData.id}/${projectData.avatar}`}
+					alt="Project avatar preview"
+					class="h-60 w-60 rounded-lg"
+				/>
+			{:else}
+				<div class="h-60 w-60 rounded-md bg-gray-300" />
+			{/if}
+			<p class="mt-2 text-center text-lg">{projectData.title}</p>
+		</div>
+		<div class="w-2/3">
+			<div class="mb-2">
+				<div class="flex justify-between">
+					<p class="projectDetail">{projectData.description}</p>
+				</div>
+			</div>
+			<div class="flex">
+				{#each projectData.tags as tag}
+					<p class="mr-1.5 mt-2 rounded-sm bg-blue-400 px-4 py-1.5 text-sm text-blue-200">
+						{tag}
+					</p>
+				{/each}
+			</div>
+		</div>
+	</div>
 </section>
--- src/routes/(projects)/projects/[projectId]/+page.ts
+++ src/routes/(projects)/projects/[projectId]/+page.ts
@@ -1,8 +1,13 @@
 import type { Load } from '@sveltejs/kit';
+import PocketBase, { Record } from 'pocketbase';
 
-export const prerender = true;
+// export const prerender = true;
 // export const csr = false;
+export const ssr = false;
 
-export const load: Load = ({ params }) => {
-	return params;
+const db = new PocketBase(import.meta.env.VITE_PUBLIC_SERVER_URL);
+
+export const load: Load = async ({ params }) => {
+	const projectData: Record = await db.collection('projects').getOne(params.projectId ?? '');
+	return { params, projectData };
 };
