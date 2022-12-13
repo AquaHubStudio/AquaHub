8a8d9bd62a1278841903f76e77efae39c400eac6
Jannis Milz <jannis.milz@econ.uzh.ch>
2022-12-13 13:53:57 +0000

    Add Beta Banner

---
+++ src/lib/components/BetaBanner.svelte
@@ -0,0 +1,5 @@
+<div class="w-full bg-blue-400 py-3 text-center text-white">
+	🔥 <span class="font-bold">AquaHub</span> is currently in <span class="font-bold">Beta</span>! Share your
+	<a class="italic underline" href="https://discord.gg/GjJNf7zhgy" target="blank_">feedback</a> or report
+	<a class="italic underline" href="https://github.com/AquaHubStudio/AquaHub/issues" target="blank_">bugs</a>! 🔥
+</div>
--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -2,9 +2,11 @@
 	import Header from '$lib/components/Header.svelte';
 	import '../../app.css';
 	import Footer from '$lib/components/Footer.svelte';
+	import BetaBanner from '$lib/components/BetaBanner.svelte';
 </script>
 
 <div class="app">
+	<BetaBanner />
 	<Header maxWidth="landing" />
 	<main>
 		<slot />
