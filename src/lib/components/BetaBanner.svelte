83f0fc69cea1bcde99c1dc2fb167aa4ad05b0d78
Jannis Milz <jannis.milz@econ.uzh.ch>
2022-12-21 09:35:24 +0000

    Change font size

--- src/lib/components/BetaBanner.svelte
+++ src/lib/components/BetaBanner.svelte
@@ -1,4 +1,4 @@
-<div class="w-full bg-blue-400 py-3 text-center text-white">
+<div class="w-full bg-blue-400 py-3 text-center text-sm text-white">
 	🔥 <span class="font-bold">AquaHub</span> is currently in <span class="font-bold">Beta</span>! Share your
 	<a class="italic underline" href="https://discord.gg/GjJNf7zhgy" target="blank_">feedback</a> or report
 	<a class="italic underline" href="https://github.com/AquaHubStudio/AquaHub/issues" target="blank_">bugs</a>! 🔥
